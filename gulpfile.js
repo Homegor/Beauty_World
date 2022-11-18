160deea95f84b70b8c27df7b00321471bedeba6c
HOME <homegor14@gmail.com>
2022-11-18 08:35:14 +0700

    wow

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
-const {src, dest} = require('gulp');
+const {src, dest, series} = require('gulp');
 const gulp = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const del = require('del')
+const sync = require('browser-sync')
+const {watch} = require("browser-sync");
 
+//Работа с SCSS
 function scss() {
-    return gulp.src('src/style/**.scss')
+    return src('src/style/**/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest('dist/css'))
         .pipe(gulp.dest('src/style/css'));
 }
+//Работа с HTML
+function html(){
+    return src('src/**/*.html')
+        .pipe(dest('dist'));
+}
+//Работа с media
+function media(){
+    return src('src/media/*.*')
+        .pipe(dest('dist/media'))
+        .pipe(gulp.dest('src/media'));
+}
+function clear(){
+    return del('dist');
+}
+function serve(){
+    sync.init({
+        server: './dist'
+    })
+    watch('src/**.html', series(html)).on('change', sync.reload)
+    watch('src/style/**.scss', series(scss)).on('change', sync.reload)
+}
 
-exports.scss = scss;
+exports.serve = series(clear, html, scss, media, serve);
