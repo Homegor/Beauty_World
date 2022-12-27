ddaa5b36cd855033f8ba880e6a9a65f8183a4ff8
HOME <homegor14@gmail.com>
2022-12-28 00:11:52 +0700

    tabs

--- app/js/app.js
+++ app/js/app.js
@@ -24,3 +24,19 @@ menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active');
 })
 // Hamburger end
+
+// Tabs start
+function openTab(evt, tabName) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("name");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabs__link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
+// Tabs end
